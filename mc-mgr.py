845d818cf04117ac06c55ec3a02ac93a0a87c8b2
paradox89 <72633854+89paradox@users.noreply.github.com>
2022-06-01 11:29:43 -0500

    Create mc-mgr.py

---
+++ mc-mgr.py
@@ -0,0 +1,12 @@
+# minecraft manager for softserve
+import os
+if os.path.exists("~/paper-1.18.2-359.jar"):
+  os.system("java -jar ~/paper-1.18.2-359.jar")
+  exit()
+else:
+  print("Downloading MC..")
+  os.system("cd ~ && wget https://api.papermc.io/v2/projects/paper/versions/1.18.2/builds/359/downloads/paper-1.18.2-359.jar")
+  print("Running..")
+  os.system("java -jar ~/paper-1.18.2-359.jar")
+  exit()
+exit()
