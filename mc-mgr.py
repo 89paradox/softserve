dfe8975bcb245252d0373d848f788691ba586575
paradox89 <72633854+89paradox@users.noreply.github.com>
2022-06-01 12:37:41 -0500

    Update mc-mgr.py

--- mc-mgr.py
+++ mc-mgr.py
@@ -1,12 +1,14 @@
 # minecraft manager for softserve
 import os
 if os.path.exists("paper-1.18.2-359.jar"):
+  os.system('echo "eula=true" >> eula.txt')
   os.system("java -jar paper-1.18.2-359.jar")
   exit()
 else:
   print("Downloading MC..")
   os.system("wget https://api.papermc.io/v2/projects/paper/versions/1.18.2/builds/359/downloads/paper-1.18.2-359.jar")
   print("Running..")
+  os.system('echo "eula=true" >> eula.txt')
   os.system("java -jar paper-1.18.2-359.jar")
   exit()
 exit()
